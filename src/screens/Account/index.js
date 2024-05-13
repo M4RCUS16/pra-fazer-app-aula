c563143c2f5212e60cb6fc44c2e95492020f0368
Marcus Vinicius Feitosa Rangel <221.14.042@EDU.gruposete.local>
2024-05-13 20:16:46 -0300

    Inserido Login

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,10 +1,32 @@
+import { firebase } from '../../services/firebaseConfig'
+import { getAuth, signOut } from "firebase/auth"
 import { View, Text } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import styles from './style';
 
-export default function Account() {
+export default function Account({navigation}) {
+  const auth = getAuth();
+
+  function logoff() {
+    signOut(auth).then(() => {
+      navigation.navigate('Login')
+    }).catch((error) => {
+    });
+  }
+
   return (
-    <View>
-      <Text>Account</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Dados do Usuário</Text>
+      <Text style={styles.info}>Nome</Text>
+      <Text style={styles.info}>E-mail</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={logoff}
+      >
+        <Text style={styles.textButton}>Sair</Text>
+      </TouchableOpacity>
     </View>
   )
 }
